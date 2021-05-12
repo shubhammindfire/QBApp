660c509f207663ab78d88527e6b1a505e8e184ac
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-05-12 11:31:56 +0530

    Feature: Added password encoding

--- backend/symfony_api/api/src/Entity/User.php
+++ backend/symfony_api/api/src/Entity/User.php
@@ -8,6 +8,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -21,7 +22,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * )
  * @UniqueEntity("username")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -237,5 +238,46 @@ class User
         $this->refreshTokenExpiresAt = $refreshTokenExpiresAt;
 
         return $this;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return ['ROLE_USER'];
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return string[] The user roles
+     */
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
     }
 }
---
+++ backend/symfony_api/api/src/EventSubscriber/PasswordHashSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class PasswordHashSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]];
+    }
+
+    public function hashPassword(ViewEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        // not the user or the method is not post then do nothing
+        if (!$user instanceof User || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        // if user and method is post then encode the password
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+    }
+}
