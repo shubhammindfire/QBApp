d3d3028960e0eaca5d82ea8ee054785084b689c1
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-05-13 20:29:59 +0530

    Feature: Added Item Entity

---
+++ backend/symfony_api/api/src/Entity/Item.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ItemRepository::class)
+ */
+class Item
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="itemId",type="integer")
+     */
+    private $itemId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(name="salesPrice",type="float")
+     */
+    private $salesPrice;
+
+    /**
+     * @ORM\Column(name="costPrice",type="float")
+     */
+    private $costPrice;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(name="createdAt",type="bigint")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(name="updatedAt",type="bigint")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(name="userId",type="string", length=225)
+     */
+    private $userId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemId(): ?int
+    {
+        return $this->itemId;
+    }
+
+    public function setItemId(int $itemId): self
+    {
+        $this->itemId = $itemId;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSalesPrice(): ?float
+    {
+        return $this->salesPrice;
+    }
+
+    public function setSalesPrice(float $salesPrice): self
+    {
+        $this->salesPrice = $salesPrice;
+
+        return $this;
+    }
+
+    public function getCostPrice(): ?float
+    {
+        return $this->costPrice;
+    }
+
+    public function setCostPrice(float $costPrice): self
+    {
+        $this->costPrice = $costPrice;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?string
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(string $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?string
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(string $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getUserId(): ?string
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(string $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+}
