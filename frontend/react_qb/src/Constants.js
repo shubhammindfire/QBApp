fc79ebcd3f3d55cdb7059b529d0a1466de317fc2
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-05-21 11:20:32 +0530

    Refactor: Moved all server urls to a Constants.js file

---
+++ frontend/react_qb/src/Constants.js
@@ -0,0 +1,4 @@
+export const BASE_SERVER_URL = "http://localhost:8000";
+export const REGISTER_URL = BASE_SERVER_URL + "/api/users";
+export const LOGIN_URL = BASE_SERVER_URL + "/api/login";
+export const CONNECT_TO_QBO_URL = BASE_SERVER_URL + "/qb/connect";
