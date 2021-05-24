05c986b981e064df738a27362a13a3ba9dc8a698
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-05-24 15:10:59 +0530

    Refactor: Added extra small(xs) breakpoint for mobile responsiveness

--- frontend/react_qb/tailwind.config.js
+++ frontend/react_qb/tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        screens: {
+            xs: "300px",
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
