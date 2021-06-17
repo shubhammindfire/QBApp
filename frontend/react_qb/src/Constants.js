62fc75c94cee4dd662c70d128d51d5756d59d50c
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-06-17 14:41:39 +0530

    changes symfony api url in constants.js

--- frontend/react_qb/src/Constants.js
+++ frontend/react_qb/src/Constants.js
@@ -1,5 +1,8 @@
 // server urls
-export const BASE_SERVER_URL = "http://localhost:8000";
+// FOR PROD ENV
+export const BASE_SERVER_URL = "http://backend.qbapp.com";
+// FOR DEV ENV
+// export const BASE_SERVER_URL = "http://localhost:8000";
 export const REGISTER_URL = BASE_SERVER_URL + "/api/users";
 export const LOGIN_URL = BASE_SERVER_URL + "/api/login";
 export const CONNECT_TO_QBO_URL = BASE_SERVER_URL + "/qb/connect";
